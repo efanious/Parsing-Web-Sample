4f0d40a4581e418213603e0f2fb676036252086c
Efana Usua <Efana.usua@wemabank.com>
2021-02-19 10:02:59 +0100

    code

---
+++ app/src/main/java/com/example/parsingwebsample/ParseItem.java
@@ -0,0 +1,18 @@
+package com.example.parsingwebsample;
+
+public class ParseItem {
+
+    String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ParseItem(String title) {
+        this.title = title;
+    }
+}
